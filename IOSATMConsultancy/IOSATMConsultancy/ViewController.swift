a69af5f43aaee7a7076ff575e6fe7eda9ba51f6a
MojoMiojo <mjasousa@sga.pucminas.br>
2022-06-22 19:50:55 -0300

    Create Project

---
+++ IOSATMConsultancy/IOSATMConsultancy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSATMConsultancy
+//
+//  Created by Mozart Sousa on 22/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
